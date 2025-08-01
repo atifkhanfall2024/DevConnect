3a49b20fa4d8fcfbc0270076d778d7143259600b
Muhammad Atif khan <22pwbcs0906@uetpeshawar.edu.pk>
2025-08-01 18:48:19 +0500

    update db

--- src/config/Db.js
+++ src/config/Db.js
@@ -6,7 +6,8 @@ const Passward= process.env.passward
   //  console.log(Passward);
 // we already know that mongoose will return us a promise so we need to handle it properly
 const ConnectDb = async()=>{
-    await Mongoose.connect(process.env.Connection-string)
+    await Mongoose.connect(`mongodb://muhammadatifkhan:ukjJEVg58zUN3roM@atifnodejs-shard-00-00.zjo3x.mongodb.net:27017,atifnodejs-shard-00-01.zjo3x.mongodb.net:27017,atifnodejs-shard-00-02.zjo3x.mongodb.net:27017/DevConnect?ssl=true&replicaSet=atlas-10eloi-shard-0&authSource=admin&retryWrites=true&w=majority
+`)
 }
 
 module.exports = ConnectDb
