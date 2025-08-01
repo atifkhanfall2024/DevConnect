43d2a551c6ee2ab919d3641ac999306627c427f2
Muhammad Atif khan <22pwbcs0906@uetpeshawar.edu.pk>
2025-08-01 18:59:58 +0500

    Adding .env

--- src/config/Db.js
+++ src/config/Db.js
@@ -1,13 +1,12 @@
 const Mongoose = require('mongoose')
 require('dotenv').config(); 
 
-const Passward= process.env.passward
+
 
   //  console.log(Passward);
 // we already know that mongoose will return us a promise so we need to handle it properly
 const ConnectDb = async()=>{
-    await Mongoose.connect(`mongodb://muhammadatifkhan:ukjJEVg58zUN3roM@atifnodejs-shard-00-00.zjo3x.mongodb.net:27017,atifnodejs-shard-00-01.zjo3x.mongodb.net:27017,atifnodejs-shard-00-02.zjo3x.mongodb.net:27017/DevConnect?ssl=true&replicaSet=atlas-10eloi-shard-0&authSource=admin&retryWrites=true&w=majority
-`)
+    await Mongoose.connect(process.env.Connection_string)
 }
 
 module.exports = ConnectDb
